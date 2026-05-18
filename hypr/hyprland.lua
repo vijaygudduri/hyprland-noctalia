df72d4106bb6ebe1d9c3d034398d753174049f46
vijaygudduri <61739941+vijaygudduri@users.noreply.github.com>
2026-05-18 14:29:46 +0530

    Set numlock to be enabled by default

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -241,7 +241,7 @@ hl.config({
         kb_model   = "",
         kb_options = "",
         kb_rules   = "",
-
+        numlock_by_default = true,
         follow_mouse = 1,
 
         sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
